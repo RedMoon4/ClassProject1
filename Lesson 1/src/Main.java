116b2c323dd7e640dec4be64a9d9a86cd8632c6e
RedMoon4 <53101643+RedMoon4@users.noreply.github.com>
2019-09-17 10:37:55 -0700

    FirstAssignment

---
+++ Lesson 1/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println((2.0 + 4) * 8/2);
+        System.out.println(Math.sqrt(576));
+        System.out.println(36.0 * 4/6);
+        System.out.println(Math.sqrt(Math.pow(2, 4) * Math.pow(2,3) * 4.5));
+        System.out.println((Math.sqrt(Math.pow(6, 8)) - (720)) / 24);
+    }
+}
